f609c6087377995bc000d6020b764c1f5495c526
SoundWave431 <vukst4n0j3v1c@gmail.com>
2018-10-22 23:11:07 -0700

    uploading site

---
+++ script.js
@@ -0,0 +1,7 @@
+var firstName = prompt ("Enter first name")
+var lastName = prompt ("Enter last name")
+if (firstName === "Andjela" && lastName === "Vuckovic"){
+	alert ("Vuk loves you very much! <3")
+}
+else {alert ("GTFO let Andjela type!")
+}
