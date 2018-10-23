dfb5b28d486e926fef0184d56cde0980d46e7f87
SoundWave431 <vukst4n0j3v1c@gmail.com>
2018-10-23 00:28:06 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var firstName = prompt ("Enter first name")
 var lastName = prompt ("Enter last name")
-if (firstName === "Andjela" && lastName === "Vuckovic"){
+if (firstName === "Andjela" || firstName === "andjela" && lastName === "Vuckovic" || lastName === "vuckovic"){
 	alert ("Vuk loves you very much! <3")
 }
 else {alert ("GTFO let Andjela type!")
